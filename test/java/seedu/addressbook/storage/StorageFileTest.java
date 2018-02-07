de54602d03e2832b2e2d7459a1cd16f4b4ec7ad3
Sherlyn <sherlyn.xinler@gmail.com>
2018-02-07 18:20:53 +0800

    Updated test cases for JUnit

--- test/java/seedu/addressbook/storage/StorageFileTest.java
+++ test/java/seedu/addressbook/storage/StorageFileTest.java
@@ -112,12 +112,12 @@ public class StorageFileTest {
         ab.addPerson(new Person(new Name("John Doe"),
                                 new Phone("98765432", false),
                                 new Email("johnd@gmail.com", false),
-                                new Address("John street, block 123, #01-01", false),
+                                new Address("Block 123, John street, #01-01, 123456", false),
                                 new UniqueTagList(Collections.emptySet())));
         ab.addPerson(new Person(new Name("Betsy Crowe"),
                                 new Phone("1234567", true),
                                 new Email("betsycrowe@gmail.com", false),
-                                new Address("Newgate Prison", true),
+                                new Address("Block 456, Newgate Prison, #01-01, 123456", true),
                                 new UniqueTagList(new Tag("friend"), new Tag("criminal"))));
         return ab;
     }
