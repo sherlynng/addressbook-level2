a922a2cbeadc41983904640cd10f587d406f423c
Sherlyn <sherlyn.xinler@gmail.com>
2018-02-14 10:33:50 +0800

    Update command class

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -7,7 +7,7 @@ import seedu.addressbook.ui.Formatter;
 
 import java.util.List;
 
-import static seedu.addressbook.ui.TextUi.DISPLAYED_INDEX_OFFSET;
+import static seedu.addressbook.ui.Formatter.DISPLAYED_INDEX_OFFSET;
 
 /**
  * Represents an executable command.
